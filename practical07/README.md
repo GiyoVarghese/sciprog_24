cd6f11e78caf1ccc855a0cc23d4d3147f1e32817
Giyo Varghese <giyovarghesetl@gmail.com>
2024-11-24 14:29:52 +0000

    Add practical07 Codes

---
+++ practical07/README.md
@@ -0,0 +1,9 @@
+This folder contains Practical 7 Codes
+
+Comple:
+gcc -o finde finde.c
+gcc -o pointers pointers.c
+
+Execute:
+./finde
+./pointers
---
+++ practical07/finde.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int factorial(int n);
+
+int main(void){
+
+	int i, order;
+	double e, *terms;
+
+	printf("Enter the required polynomial order\n");
+	if(scanf("%d",&order) !=1)
+	{
+		printf("Did not a number\n");
+		return 1;
+	}
+
+	//Allocate space depending on n
+	terms = (double*)malloc(order*sizeof(double));
+	for(i=0;i<order;i++){
+		terms[i] = 1.0/ (double)factorial(i+1);
+		printf("%.16lf\n",terms[i]);
+	}
+	e = 1.0;
+	for(i=0;i<order;i++){
+		e = e+terms[i];
+	}
+	free(terms);
+
+	printf("e is estimated as %.10lf, with difference %e\n",e,e-exp(1.0));
+	return 0;
+}
+
+int factorial(int n){
+	if(n<0){
+		printf("Error, Negative number is passed to factorial\n");
+	}
+	else if(n==0){
+		return 1;
+	}
+	else{
+		return(n*factorial(n-1));
+	}
+}
